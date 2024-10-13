c9953ae6bf5cf6cd792f95cbbc6e02f67d6bd47c
Pedro-Ferreira-89 <81267194+Pedro-Ferreira-89@users.noreply.github.com>
2024-10-13 16:18:48 +0100

    Add more comments

--- src/main/java/Stake.java
+++ src/main/java/Stake.java
@@ -13,19 +13,26 @@ import java.util.Map;
 import static io.nuls.contract.sdk.Utils.emit;
 import static io.nuls.contract.sdk.Utils.require;
 
-
 /**
- * @title Nuls Oracles
+ * @title   Nuls Oracles Revenue Distribution Contract
  *
+ * @dev     Allows for users to deposit Nuls Oracle Tokens (ORA)
+ *          and earn revenue from the Nuls Oracles Project.
+ *          After deposited users will no longer be able to
+ *          withdraw the tokens, meanign that by depositing
+ *          they are burning their tokens.
  *
- * */
-public class Stake extends Ownable implements Contract{
+ * @author  Pedro G. S. Ferreira
+ *
+ */
+public class Stake implements Contract{
 
     /// Constants
-    private static long DURATION = 86400 * 2;           // Rewards distribution period
+    private static long DURATION = 86400 * 2;       // Rewards distribution period
+
+    private static BigInteger MIN_NULS_AMOUNT = BigInteger.valueOf(1_000_000);    // Minimum Nuls transferable amount
 
     /// Variables
-    private Address rewardsToken;                       // Reward Token
     private Address stakingToken;                       // Staking Token
     private long    lastUpdateTime;                     // Last time when rewards were updated
     private Address treasury;                           // Treasury Address that will receive Contract Revenue
@@ -38,9 +45,8 @@ public class Stake extends Ownable implements Contract{
     private BigInteger  _totalSupply          = BigInteger.ZERO;    // Total upply deposited in Contract
 
     private Map<Address, BigInteger> userRewardPerTokenPaid = new HashMap<Address, BigInteger>(); // User reward per token deposited
-    private Map<Address, BigInteger> rewards                = new HashMap<Address, BigInteger>();
-    private Map<Address, BigInteger> totalRewards           = new HashMap<Address, BigInteger>();
-    private Map<Address, BigInteger> _balances              = new HashMap<Address, BigInteger>();
+    private Map<Address, BigInteger> rewards                = new HashMap<Address, BigInteger>(); // Rewards Earned by User
+    private Map<Address, BigInteger> _balances              = new HashMap<Address, BigInteger>(); // User ORA Tokens Deposited
 
     /**
      * Constructor
@@ -49,23 +55,11 @@ public class Stake extends Ownable implements Contract{
      * @param rewardTokenAddress Token Reward Address
      * @param treasury  Treasury Address that will receive Contract Revenue
      * */
-    public Stake(Address depositToken, String rewardTokenAddress, , Address treasury ) {
+    public Stake(Address depositToken, String rewardTokenAddress, Address treasury ) {
 
         stakingToken            = depositToken;
-        rewardsToken            = new Address(rewardTokenAddress);
         this.treasury           = treasury;
         rewardDistribution      = Msg.sender();
-        allowWithdrawWithouLock = false;
-    }
-
-    protected void nonReentrant(){
-        require(!locked, "Already Entered");
-        locked = true;
-    }
-
-    protected void closeReentrant(){
-        require(locked, "Not Entered");
-        locked = false;
     }
 
     /*===========================================
@@ -75,17 +69,100 @@ public class Stake extends Ownable implements Contract{
      ===========================================*/
 
     /**
+     * Returns rewards distribution period
      *
-     * @return All Nuls Oracle Tokens staked
+     * @return Rewards distribution period
+     */
+    @View
+    public long rewardsDistributionPeriod() {
+        return DURATION;
+    }
+
+    /**
+     * Returns Nuls Oracle Tokens Address
+     *
+     * @return User Nuls Oracle Token (ORA) Address
+     */
+    @View
+    public Address getStakingToken(){
+        return stakingToken;
+    }
+
+    /**
+     * Returns Treasury Address
+     *
+     * @return treasury address
+     */
+    @View
+    public Address getTreasuryAddress(){
+        return treasury;
+    }
+
+    /**
+     * Returns Rewards Distrbution/Admin Address
+     *
+     * @return Rewards distribution/Admin address
+     */
+    @View
+    public Address getRewardDistribution(){
+        return rewardDistribution;
+    }
+
+    /**
+     * Returns Lock Status
+     *
+     * @return lock status
+     */
+    @View
+    public boolean getLockStatus(){
+        return locked;
+    }
+
+    /**
+     * Returns all the Nuls Oracle Tokens (ORA) deposited
+     *
+     * @return All Nuls Oracle Tokens deposited
      */
     @View
     public BigInteger totalSupply() {
         return _totalSupply;
     }
 
+
     /**
+     *  Returns when rewards period finishes
      *
-     * @return All NSWAP earned in staking
+     * @return when rewards period finishes
+     */
+    @View
+    public long getPeriodFinish() {
+        return periodFinish;
+    }
+
+    /**
+     * Returns Reward Rate per second
+     *
+     * @return reward rate per second
+     */
+    @View
+    public BigInteger getRewardRate() {
+        return rewardRate;
+    }
+
+    /**
+     * Returns Reward Rate per token deposited
+     *
+     * @return reward rate per token deposited
+     */
+    @View
+    public BigInteger getRewardPerTokenStored() {
+        return rewardPerTokenStored;
+    }
+
+    /**
+     *  Returns earned Nuls
+     *
+     * @return Nuls earned in revenue distribution
      */
     @View
     public BigInteger earned(Address account) {
@@ -93,14 +170,20 @@ public class Stake extends Ownable implements Contract{
     }
 
     /**
+     *  Returns When rewards were last distributed
      *
-     * @return The
+     * @return  when rewards were last distributed
      */
     @View
-    public BigInteger lockedTime(Address account) {
-        return lockTime.get(account);
+    public long lastTimeRewardUpdated() {
+        return lastUpdateTime;
     }
 
+    /**
+     *  Returns Until when rewards will last
+     *
+     * @return until when rewards will last
+     */
     @View
     public long lastTimeRewardApplicable() {
         long timestamp = Block.timestamp();
@@ -108,9 +191,40 @@ public class Stake extends Ownable implements Contract{
     }
 
     /**
+     * Returns user reward per token deposited
      *
-     * @param account
-     * @return
+     * @param account User address
+     * @return user reward per token deposited
+     */
+    @View
+    public BigInteger getUserRewardPerTokenPaid(Address account) {
+        if (userRewardPerTokenPaid.get(account) != null) {
+            return userRewardPerTokenPaid.get(account);
+        } else {
+            return BigInteger.ZERO;
+        }
+    }
+
+    /**
+     * Returns user rewards already allocated to user
+     *
+     * @param account User address
+     * @return user rewards already allocated to user
+     */
+    @View
+    public BigInteger getUserAlreadyStoredRewards(Address account) {
+        if (rewards.get(account) != null) {
+            return rewards.get(account);
+        } else {
+            return BigInteger.ZERO;
+        }
+    }
+
+    /**
+     * Returns Nuls Oracle Tokens (ORA) balance
+     *
+     * @param account User address
+     * @return User Nuls Oracle Tokens (ORA) balance
      */
     @View
     public BigInteger _balanceOf(Address account) {
@@ -127,40 +241,27 @@ public class Stake extends Ownable implements Contract{
 
      ===========================================*/
 
-    public void setStakingToken(Address newToken){
-        onlyOwner();
-        require(newToken != null, "Invalid Address");
-        stakingToken = newToken;
+    /**
+     * @dev Locks Contract in order to prevent reentrancy attacks
+     * */
+    protected void nonReentrant(){
+        require(!locked, "Already Entered");
+        locked = true;
     }
-
-    public void setTreasury(Address addr){
-        onlyOwner();
-        require(addr != null, "Invalid Treasury Address");
-        treasury = addr;
-    }
-
-    @View
-    public Address getStakingToken(){
-        return stakingToken;
-    }
-
-    private void onlyRewardDistribution() {
-        require(Msg.sender().equals(rewardDistribution), "Caller is not reward distribution");
-    }
-
-    /*===========================================
-
-      SETTERS
-
-     ===========================================*/
 
     /**
-     *
-     * @param _rewardDistribution
-     */
-    public void setRewardDistribution(Address _rewardDistribution) {
-        onlyRewardDistribution();
-        rewardDistribution = _rewardDistribution;
+     * @dev Unlocks Contract after vulnerable operations are made
+     * */
+    protected void closeReentrant(){
+        require(locked, "Not Entered");
+        locked = false;
+    }
+
+    /**
+     * @dev Only allow Rewards Distribution Address to call function
+     * */
+    private void onlyRewardDistribution() {
+        require(Msg.sender().equals(rewardDistribution), "Caller is not reward distribution");
     }
 
     /*===========================================
@@ -174,55 +275,74 @@ public class Stake extends Ownable implements Contract{
     public void _payable() { notifyRewardAmount(Msg.value()); }
 
     /**
+     *  Deposits Nuls Oracle Tokens (ORA) in the Contract
+     *  and updated user balances
      *
-     * @param amount Amount of ORA Tokens to Stake
+     * @param amount Amount of ORA Tokens to deposit
      */
     public void stake(BigInteger amount) {
 
+        // Prevent Reentrancy Attacks
         nonReentrant();
 
-        //Update rewards in order to avoid rewards conflits
+        // Update rewards in order to avoid rewards conflits
         updateReward(Msg.sender());
 
-        //Require that the amount is bigger than 0
+        // Require that the amount to deposit is bigger than 0
         require(amount.compareTo(BigInteger.ZERO) > 0, "Cannot stake 0");
 
-        //Get user allowance and check if it is bigger than the amount to stake
+        // Get user allowance and check if it is bigger than the amount to stake
         BigInteger allowance = getUserAllowance(stakingToken, Msg.sender(), Msg.address());
-
         require(allowance.compareTo(amount) >= 0, "Low Allowance");
 
-        //Transfer ORA tokens from user to this contract
+        // Transfer ORA tokens from user to this contract
         safeTransferFrom(stakingToken, Msg.sender(), Msg.address(), amount);
-        //Add the stake amount to the total added
+
+        // Add the stake amount to the total added
         _totalSupply = _totalSupply.add(amount);
-        //Check if the user balance exists and adds the stake amount to the all user staked
+
+        // Check if the user balance exists and adds the stake amount to the all user staked
         if (_balances.get(Msg.sender()) != null) {
             _balances.put(Msg.sender(), _balances.get(Msg.sender()).add(amount));
         } else {
             _balances.put(Msg.sender(), amount);
         }
 
-        //Emit event with the Stake event
+        // Emit event with the Stake event
         emit(new Staked(Msg.sender(), amount));
 
+        // Close Reentrancy Attacks Prevention
         closeReentrant();
     }
 
-    private BigInteger getReward() {
 
+    /**
+     *  Get Nuls rewards from Nuls Oracle Project
+     *  revenue distribution
+     *
+     * @return nuls earned by user
+     * */
+    public BigInteger getReward() {
+
+        // Prevent Reentrancy Attacks
         nonReentrant();
 
+        // Update user rewards to allow user to claim all earned rewards
         updateReward(Msg.sender());
 
+        // Get amount of rewards
         BigInteger trueReward = rewards.get(Msg.sender());
 
-        if (trueReward.compareTo(BigInteger.ZERO) > 0) {
+        // If rewards are higher than min nuls transferable amount then send rewards
+        if (trueReward.compareTo(MIN_NULS_AMOUNT) >= 0) {
+
             rewards.put(Msg.sender(), BigInteger.ZERO);
             Msg.sender().transfer(trueReward);
+
             emit(new RewardPaid(Msg.sender(), trueReward));
         }
 
+        // Close Reentrancy Attacks Prevention
         closeReentrant();
 
         return trueReward;
@@ -231,20 +351,24 @@ public class Stake extends Ownable implements Contract{
 
 
     /**
+     * Distribute revenue through depositers
      *
-     * @param reward
+     * @param reward Amount of revenue to distribute
      */
     @Payable
     public void notifyRewardAmount(BigInteger reward) {
 
+        // Prevent Reentrancy Attacks
         nonReentrant();
 
-        require(Msg.value().compareTo(reward) >= 0)
+        require(Msg.value().compareTo(reward) >= 0, "Insufficient Amount");
 
         //Update last time disributed rewards
         updateReward(null);
 
-        //
+        // If rewards period already finished or not even started
+        // then create a new period, if otherwise
+        // then increment rewards to the current rewards
         if (Block.timestamp() >= periodFinish) {
 
             rewardRate = reward.divide(BigInteger.valueOf(DURATION));
@@ -254,13 +378,16 @@ public class Stake extends Ownable implements Contract{
             BigInteger remaining = BigInteger.valueOf(periodFinish).subtract(BigInteger.valueOf(Block.timestamp()));
             BigInteger leftover = remaining.multiply(rewardRate);
             rewardRate = reward.add(leftover).divide(BigInteger.valueOf(DURATION));
+
         }
 
+        // Update last revenue distribution time and revenue distribution period
         lastUpdateTime = Block.timestamp();
         periodFinish = Block.timestamp() + DURATION;
 
         emit(new RewardAdded(reward));
 
+        // Close Reentrancy Attacks Prevention
         closeReentrant();
 
     }
@@ -271,6 +398,43 @@ public class Stake extends Ownable implements Contract{
 
      ===========================================*/
 
+    /**
+     * Set New Staking Token Address
+     *
+     * @param newToken new Staking Token Address
+     */
+    public void setStakingToken(Address newToken){
+        onlyRewardDistribution();
+        require(newToken != null, "Invalid Address");
+        stakingToken = newToken;
+    }
+
+    /**
+     * Set New Treasury Address
+     *
+     * @param addr new Treasury Address
+     */
+    public void setTreasury(Address addr){
+        onlyRewardDistribution();
+        require(addr != null, "Invalid Treasury Address");
+        treasury = addr;
+    }
+
+    /**
+     *  Set New Rewards Distribution/Admin Address
+     *
+     * @param _rewardDistribution new rewards distrbution/admin address
+     */
+    public void setRewardDistribution(Address _rewardDistribution) {
+        onlyRewardDistribution();
+        rewardDistribution = _rewardDistribution;
+    }
+
+    /**
+     * Recover Token funds lost in contract
+     *
+     * @param tkn_ Token Address of assets to be recovered
+     */
     public void recoverNRC20(Address tkn_) {
         //Only rewarder address can give reward
         onlyRewardDistribution();
@@ -283,12 +447,30 @@ public class Stake extends Ownable implements Contract{
         safeTransfer(tkn_, rewardDistribution, b);
     }
 
+    /**
+     * Recover Nuls funds lost in contract
+     */
+    public void recoverNuls() {
+        //Only rewarder address can give reward
+        onlyRewardDistribution();
+
+        Msg.sender().transfer(Msg.address().balance());
+    }
+
+
+
     /*===========================================
 
       PRIVATE FUNCTIONS
 
      ===========================================*/
 
+    /**
+     * Updates rewards earned by user before any new operations are done,
+     *  if user is null updates rewards per token stored only
+     *
+     * @param account User Address
+     */
     private void updateReward(Address account) {
         rewardPerTokenStored = rewardPerToken();
         lastUpdateTime = lastTimeRewardApplicable();
@@ -299,8 +481,9 @@ public class Stake extends Ownable implements Contract{
     }
 
     /**
-     * Reward per token stored
-     * @return
+     * Returns all rewards per token deposited
+     *
+     * @return Rewards per token deposited
      */
     private BigInteger rewardPerToken() {
         if (_totalSupply.equals(BigInteger.ZERO)) {
@@ -315,6 +498,13 @@ public class Stake extends Ownable implements Contract{
                         divide(_totalSupply));
     }
 
+    /**
+     *  Get all the rewards earned by a user, all stored and all not stored
+     *
+     * @param account User Address
+     *
+     * @return user earned rewards
+     */
     private BigInteger _earned(Address account) {
         BigInteger userRewardPer = BigInteger.ZERO;
         if (userRewardPerTokenPaid.get(account) != null) {
@@ -329,18 +519,40 @@ public class Stake extends Ownable implements Contract{
 
     }
 
+    /**
+     *  Transfer token from this contract to recipient address
+     *
+     * @param token Token Address
+     * @param recipient Recipient Address
+     * @param amount Amount to Transfer
+     */
     private void safeTransfer(@Required Address token, @Required Address recipient, @Required BigInteger amount){
         String[][] argsM = new String[][]{new String[]{recipient.toString()}, new String[]{amount.toString()}};
         boolean b = new Boolean(token.callWithReturnValue("transfer", "", argsM, BigInteger.ZERO));
         require(b, "NulswapV1: Failed to transfer");
     }
 
+    /**
+     *  Transfer token from the address from to the recipient address
+     *
+     * @param token Token Address
+     * @param from The Address from where the tokens will be retrieved
+     * @param recipient Recipient Address
+     * @param amount Amount to Transfer
+     */
     private void safeTransferFrom(@Required Address token, @Required Address from, @Required Address recipient, @Required BigInteger amount){
         String[][] args = new String[][]{new String[]{from.toString()}, new String[]{recipient.toString()}, new String[]{amount.toString()}};
         boolean b = new Boolean(token.callWithReturnValue("transferFrom", "", args, BigInteger.ZERO));
         require(b, "NulswapV1: Failed to transfer");
     }
 
+    /**
+     *  Get address allowance to spend funds from other address
+     *
+     * @param token Token Address
+     * @param owner Holder of funds Address
+     * @param mover Address who wants to transfer funds from owner
+     */
     private BigInteger getUserAllowance(@Required Address token, @Required Address owner, @Required Address mover){
         String[][] args = new String[][]{new String[]{owner.toString()}, new String[]{mover.toString()}};
         BigInteger b = new BigInteger(token.callWithReturnValue("allowance", "", args, BigInteger.ZERO));
